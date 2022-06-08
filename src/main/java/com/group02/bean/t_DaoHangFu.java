a34c978b12bf50b1e40daf8d94fc24f008a72f78
hzq <3092270340@qq.com>
2022-06-08 17:50:41 +0800

    t_DaoHangFu具体类

---
+++ src/main/java/com/group02/bean/t_DaoHangFu.java
@@ -0,0 +1,75 @@
+package com.group02.bean;
+
+import java.sql.Date;
+
+/**
+ * 导航父表
+ */
+public class t_DaoHangFu {
+
+    /**
+     * 系统id
+     */
+    private String xiTongID;
+
+    /**
+     * 被改变
+     */
+    private Integer beiGaiBian;
+
+    /**
+     * 上级id
+     */
+    private String shangJiID;
+
+    /**
+     * 更新时间
+     */
+    private Date gengXinShiJian;
+
+    /**
+     * 组名称
+     */
+    private String zuMingCheng;
+
+    /**
+     * 排序id
+     */
+    private String paiXuID;
+
+    /**
+     * 组id
+     */
+    private String ZuID;
+
+    /**
+     * 备用01
+     */
+    private String beiYong01;
+
+    /**
+     * 备用02
+     */
+    private String bingRenID;
+
+    /**
+     * 备用03
+     */
+    private String beiYong03;
+
+    /**
+     * 备用04
+     */
+    private String beiYong04;
+
+    /**
+     * e名称
+     */
+    private String eMingCheng;
+
+    /**
+     * 名称
+     */
+    private String mingCheng;
+
+}
