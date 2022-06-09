4e90bfe759c08d5fc78896f6467c48acc89fb395
hzq <3092270340@qq.com>
2022-06-09 17:09:30 +0800

    测试

--- src/test/java/com/group02/DemoApplicationTests.java
+++ src/test/java/com/group02/DemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.group02;
 
+import com.group02.bean.DaoHangFu;
+import com.group02.serviceImpl.DaoHangFuServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+//@MapperScan(value = "com.group02.dao")
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Autowired
+//    DaoHangFuServiceImpl daoHangFuServiceImpl;
+//
+//
+//    @Test
+//    void contextLoads() {
+//
+//        List<DaoHangFu> a = daoHangFuServiceImpl.findDaoHangFuAll();
+//        for (DaoHangFu aa:a) {
+//            System.out.println(aa.getZuMingCheng());
+//        }
+
+
+
+
+
 
 }
